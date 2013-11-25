5bd4382100d97a048aca0d6f849acd4d78e27411
U-PRC-20130906ANO\Administrator <Administrator@PRC-20130906ANO.(none)>
2013-11-25 17:03:03 +0800

    2013.11.25

--- bootstrap/practice/jumbotron/jumbotron.php
+++ bootstrap/practice/jumbotron/jumbotron.php
@@ -25,9 +25,69 @@
     <![endif]-->
 	</head>
 	<body>
-		<div class="navbar" role="navigation">
+		<div class="container">			
+		<div class="navbar navbar-inverse" role="navigation">
+			<div class="navbar-header">
+				<a class="navbar-brand" href="#">project name</a>
+			</div>
+			<div >
+				<ul class="nav navbar-nav">
+					<li><a herf="#">Link</a></li>
+					<li><a herf="#">Link</a></li>
+					<li class="dropdown">
+						<a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown<b class="caret"></b></a>
+						<ul class="dropdown-menu" role="menu">
+							<li><a href="#">Action</a></li>
+							<li><a href="#">Action</a></li>
+							<li class="divider"></li>
+							<li><a href="#">Action</a></li>
+						</ul>
+					</li>
+				</ul>
+			</div>
 				
-
+			<div>
+				<form class="navbar-form navbar-right">
+					<div class="form-group">
+						<input type="text" placeholder="Email" class="form-control">
+					</div>
+					<div class="form-group">
+						<input type="password" placeholder="Password" class="form-control">
+					</div>
+					<button type="submit" class="btn btn-success">Sign in</button>
+				</form>
+			</div>
 		</div>
+		</div>
+
+			<div class="container">
+		<div class="jumbotron">
+				<h1>志不坚则智不达，言不信则行不果</h1>
+				<p>春江潮水连海平，海上明月共潮生</p>
+				<p><a class="btn btn-primary ">Learn more &raquo;</a></p>
+			</div>
+		</div>
+		
+		<div class="container">
+			<div class="row">
+				<div class="col-md-4">
+					<h2>左青龙</h2>
+					</p>must has blank?whantwhatmust has blank?whantwhat must has blank?whantwhat  </p>
+					<p><a class="btn btn-default" href="#">View details &raquo;</a></p>
+				</div>
+				<div class="col-md-4">
+					<h2>米老鼠</h2>
+					<p>会自动换行吗？lalalallalalalalalallalalalallalalallalalall alalallalalalallal</p>
+					<p><a class="btn btn-default" href="#">View details &raquo;</a></p>
+				</div>
+				<div class="col-md-4">
+					<h2>右白虎</h2>
+					<p>会自动换行吗？lalalallalalalalalallalalalallalalallalalallalalallalalalallal</p>
+					<p><a class="btn btn-default" href="#">View details &raquo;</a></p>
+				</div>
+			</div>
+		</div>
+<script src="https://code.jquery.com/jquery-1.10.2.min.js"></script>
+    <script src="../../bootstrap-3.0.0/dist/js/bootstrap.min.js"></script>
 	</body>
 </html>
